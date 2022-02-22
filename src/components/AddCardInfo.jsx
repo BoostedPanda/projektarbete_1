25e858be524c61d497bfc863c04640dd8693b68b
BoostedPanda <kevinujfalusi@hotmail.se>
2022-02-22 14:09:20 +0100

    added card functions, routes
    Co-authored-by: Tanya Damgaard <TAYNII@users.noreply.github.com>

--- src/components/AddCardInfo.jsx
+++ src/components/AddCardInfo.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux"
 import { addUser } from "../redux/userSlice"
 
-const AddCardInfo = () => {
+const AddCardInfo = (props) => {
   const dispatch = useDispatch()
-  const { user } = useSelector((state) => state.userList)
+  const { cardInformation } = useSelector((state) => state.userList)
+  const cardData = {
+    cardName: "",
+    cardNumber: "",
+    cardMonth: "",
+    cardYear: "",
+    ccv: "",
+    bankName: ""
+  };
+  const [value, setValue] = useState(cardData)
+
+
 
   const testsend = () => {
    const cardInformation = 
@@ -20,14 +31,52 @@ const AddCardInfo = () => {
     
     dispatch(addUser(cardInformation))
   }
+  
 
 
+  const testOnchange = (e) => {
+    const nextCard = {
+      ...value,
+      [e.target.name]: e.target.value
+    };
+    setValue(nextCard);
+    // document.querySelector(`${id}`).innerHTML = value
+    
+  }
 
   return (
     <div>
       <h1>Add card</h1>
+      <button onClick={() => {testsend()}}>Send user</button>
+      <button onClick={() => {console.log(cardInformation)}}>Log user</button>
+      <div id='cardplaceholder'>
+        <p id='cardNumber'>Cardnumber: {value.cardNumber}</p>
+        <p>Cardholder: {cardInformation[0].cardName}</p>
+        <p id='month'>MM/YY</p>
+        <p id='cvc'>ccv/cvc</p>
 
-      <button onClick={() => {testsend()}}>Log user</button>
+      </div>
+<form>
+        <div id='inputbox'>
+        <div>
+            <label htmlFor='cardNumber'>Card Number</label>
+            <input maxLength="16" name='cardNumber' type="text" onChange={testOnchange}/>
+        </div>
+        <div>
+            <label htmlFor='cardName'>Card Name</label>
+            <input name='cardName' type="text" disabled placeholder={cardInformation[0].cardName}/>
+        </div>
+          <select name='cardMonth' >
+            <option disabled>Month</option>
+          </select>
+          <input type="text" />
+        <div>
+            <label htmlFor='ccv'>CCV/CVC</label>
+            <input maxLength="3" name='ccv' type="text" />
+        </div>
+        </div>
+</form>
+
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import {useSelector,useDispatch} from "react-redux"
 import {getUser} from "../redux/userSlice"
-import {useEffect} from "react"
-import AddCardInfo from '../components/AddCardInfo'
+
+import {Link} from "react-router-dom"
+
 
 function Home() {
   const dispatch = useDispatch()
   const {cards, status, cardInformation} = useSelector((state) => state.userList)
 
-  useEffect(() => {
-    dispatch(getUser())
-  }, [dispatch])
 
   return (
     <div>
       <h2>Home</h2>
+      <div>
+      <Link to="/addcard"><button>Add card</button></Link>
+      </div>
       <button onClick={() => {dispatch(getUser())}}>Getuser</button>
       <button onClick={() => {console.log(cards)}}>test</button>
       <button onClick={() => {console.log(cardInformation)}}>test2</button>
-      <AddCardInfo/>
+
     </div>
   )
 }
